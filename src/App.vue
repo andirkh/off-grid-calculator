5463dc312ec95ee955317da48a1a5d6b921d9f1a
niltavalabs-dev <andi@niltavalabs.com>
2018-01-19 19:55:28 +0700

    new css

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,89 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+html, body {
+  height: 100%;
+  background: white;
+}
+
+* {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+.finder {
+  padding: 55px;
+  max-width: 65em;
+  margin: 0 auto;
+}
+
+.form-box {
+  background-color: white;
+  border-radius: 9px;
+  width: 700px;
+  min-height: 800px;
+  padding: 25px;
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+
+.left {
+  text-align: left;
+}
+
+.label-form {
+  font-size: 1em;
+  margin-bottom: 15px;
+  display: block;
+}
+
+.big-input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 2px solid #ddd;
+  height: 48px;
+  margin-bottom: 10px;
+  outline: none;
+}
+.big-input:focus {
+  border: 4px solid #00ffb9;
+}
+
+::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+  color: #777;
+}
+::-moz-placeholder { /* Firefox 19+ */
+  color: #777;
+}
+:-ms-input-placeholder { /* IE 10+ */
+  color: #777;
+}
+:-moz-placeholder { /* Firefox 18- */
+  color: #777;
+}
+
+input[type='number'] {
+    -moz-appearance:textfield;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
+@media (max-width: 940px){
+  .finder {
+    width: 100%;
+    max-width: none;
+    padding: 10px;
+  }
+
+  .form-box {
+    width: 100%;
+    padding: 15px;
+  }
+}
 </style>
--- src/components/SolarEnergy.vue
+++ src/components/SolarEnergy.vue
@@ -1,83 +1,85 @@
 <template>
-  <div class="center">
-    <h3>Produksi Daya</h3>
-    <label>Daya Solar Panel total (Watt):</label>
-    <input
-      type="number"
-      v-model.number="solarPanelWatt"
+  <div class="finder left">
+    <div class="form-box">
+      <h2>Produksi Daya</h2>
+      <label class="label-form">Daya Solar Panel total (Watt):</label>
+      <input class="big-input"
+        type="number"
+        v-model.number="solarPanelWatt"
 
-      placeholder="Daya solar panel keseluruhan setelah dirangkai" />
-    <br />
-    <label>Lama Charging per hari (sun hours/day):</label>
-    <input type="number" v-model.number="sunHours" placeholder="Lama charging dalam sehari (jam)" />
-    <p>Produksi Daya/hari : {{ dailyPowerProduction }} Wh</p>
-    <p>Produksi Daya/tahun : {{ yearlyEnergyCreation }} Wh/y</p>
+        placeholder="Daya solar panel keseluruhan setelah dirangkai" />
+      <br />
+      <label class="label-form">Lama Charging per hari (sun hours/day):</label>
+      <input class="big-input"  type="number" v-model.number="sunHours" placeholder="Lama charging dalam sehari (jam)" />
+      <p>Produksi Daya/hari : <strong>{{ dailyPowerProduction }} Wh</strong></p>
+      <p>Produksi Daya/tahun : <strong>{{ yearlyEnergyCreation }} Wh/y</strong></p>
 
-    <br />
+      <br />
 
-    <h3>Investasi: </h3>
-    <label> Harga Solar Panel: </label>
-    <input type="number" v-model.number="solarPanelPrice" placeholder="Harga Solar Panel Keseluruhan" />
-    <br />
-    <label> Harga Solar Controller: </label>
-    <input type="number" v-model.number="controllerPrice" placeholder="Harga Controller" />
-    <br />
-    <label> Harga Baterai</label>
-    <input type="number" v-model.number="batteryPrice" placeholder="Harga Baterai/Aki Keseluruhan" />
-    <br />
-    <label> Harga Inverter </label>
-    <input type="number" v-model.number="inverterPrice" placeholder="Harga Inverter" />
-    <br />
-    <label> Harga Komponen Lain </label>
-    <input type="number" v-model.number="miscPrice" placeholder="Harga Komponen Lain" />
-    <br />
-    <label>TDL PLN</label>
-    <select v-model="tdlPLN">
-      <option disabled value="">Rumah Tangga R1:</option>
-      <option v-bind:value="0.415">R-1/450 VA (Subsidi)</option>
-      <option v-bind:value="0.586">R-1/900 VA (Subsidi)</option>
-      <option v-bind:value="1.352">R-1/900 VA-RTM (Rumah Tangga Mampu)</option>
-      <option v-bind:value="1.46728">R-1/1300 VA</option>
-      <option v-bind:value="1.46728">R-1/2200 VA</option>
-      <option v-bind:value="1.46728">R-2/3500 VA, 4400 VA, 5500 VA</option>
-      <option v-bind:value="1.46728">R-3/6600 VA ke atas</option>
+      <h2>Investasi: </h2>
+      <label class="label-form"> Harga Solar Panel: </label>
+      <input class="big-input"  type="number" v-model.number="solarPanelPrice" placeholder="Harga Solar Panel Keseluruhan" />
+      <br />
+      <label class="label-form"> Harga Solar Controller: </label>
+      <input class="big-input"  type="number" v-model.number="controllerPrice" placeholder="Harga Controller" />
+      <br />
+      <label class="label-form"> Harga Baterai: </label>
+      <input class="big-input"  type="number" v-model.number="batteryPrice" placeholder="Harga Baterai/Aki Keseluruhan" />
+      <br />
+      <label class="label-form"> Harga Inverter: </label>
+      <input class="big-input"  type="number" v-model.number="inverterPrice" placeholder="Harga Inverter" />
+      <br />
+      <label class="label-form"> Harga Komponen Lain: </label>
+      <input class="big-input"  type="number" v-model.number="miscPrice" placeholder="Harga Komponen Lain" />
+      <br />
+      <label class="label-form">TDL PLN</label>
+      <select v-model="tdlPLN" class="big-input">
+        <option disabled value="">Rumah Tangga R1:</option>
+        <option v-bind:value="0.415">R-1/450 VA (Subsidi)</option>
+        <option v-bind:value="0.586">R-1/900 VA (Subsidi)</option>
+        <option v-bind:value="1.352">R-1/900 VA-RTM (Rumah Tangga Mampu)</option>
+        <option v-bind:value="1.46728">R-1/1300 VA</option>
+        <option v-bind:value="1.46728">R-1/2200 VA</option>
+        <option v-bind:value="1.46728">R-2/3500 VA, 4400 VA, 5500 VA</option>
+        <option v-bind:value="1.46728">R-3/6600 VA ke atas</option>
 
-      <option disabled value="">Bisnis B1 (Subsidi):</option>
-      <option v-bind:value="0.535">B-1/450 VA</option>
-      <option v-bind:value="0.63">B-1/900 VA</option>
-      <option v-bind:value="0.966">B-1/1300 VA</option>
-      <option v-bind:value="1.1">B-1/2200 VA</option>
-      <option v-bind:value="1.1">B-1/3500 VA</option>
-      <option v-bind:value="1.1">B-1/4400 VA</option>
-      <option v-bind:value="1.1">B-1/5500 VA</option>
+        <option disabled value="">Bisnis B1 (Subsidi):</option>
+        <option v-bind:value="0.535">B-1/450 VA</option>
+        <option v-bind:value="0.63">B-1/900 VA</option>
+        <option v-bind:value="0.966">B-1/1300 VA</option>
+        <option v-bind:value="1.1">B-1/2200 VA</option>
+        <option v-bind:value="1.1">B-1/3500 VA</option>
+        <option v-bind:value="1.1">B-1/4400 VA</option>
+        <option v-bind:value="1.1">B-1/5500 VA</option>
 
-      <option disabled value="">Sosial (Subsidi):</option>
-      <option v-bind:value="0.325">S-2/450 VA	</option>
-      <option v-bind:value="0.455">S-2/900 VA</option>
-      <option v-bind:value="0.708">S-2/1300 VA</option>
-      <option v-bind:value="0.76">S-2/2200 VA</option>
-      <option v-bind:value="0.9">S-2/3500 VA s.d 200 kVA</option>
+        <option disabled value="">Sosial (Subsidi):</option>
+        <option v-bind:value="0.325">S-2/450 VA	</option>
+        <option v-bind:value="0.455">S-2/900 VA</option>
+        <option v-bind:value="0.708">S-2/1300 VA</option>
+        <option v-bind:value="0.76">S-2/2200 VA</option>
+        <option v-bind:value="0.9">S-2/3500 VA s.d 200 kVA</option>
 
-      <option disabled value="">Industri (Subsidi):</option>
-      <option v-bind:value="0.485">I-1/450 VA</option>
-      <option v-bind:value="0.6">I-1/900 VA</option>
-      <option v-bind:value="0.93">I-1/1300 VA</option>
-      <option v-bind:value="0.96">I-1/2200 VA</option>
-      <option v-bind:value="1.112">I-1/3500 VA s.d 14 kVA</option>
+        <option disabled value="">Industri (Subsidi):</option>
+        <option v-bind:value="0.485">I-1/450 VA</option>
+        <option v-bind:value="0.6">I-1/900 VA</option>
+        <option v-bind:value="0.93">I-1/1300 VA</option>
+        <option v-bind:value="0.96">I-1/2200 VA</option>
+        <option v-bind:value="1.112">I-1/3500 VA s.d 14 kVA</option>
 
-      <option disabled value="">Publik (Subsidi):</option>
-      <option v-bind:value="0.685">P-1/450 VA</option>
-      <option v-bind:value="0.76">P-1/900 VA</option>
-      <option v-bind:value="1.049">P-1/1300 VA</option>
-      <option v-bind:value="1.076">P-1/2200 VA</option>
-      <option v-bind:value="1.076">P-1/3500 VA</option>
-      <option v-bind:value="1.076">P-1/4400 VA</option>
-      <option v-bind:value="1.076">P-1/5500 VA</option>
-    </select>
-    <br />
-    <p>Total Biaya Sistem: {{ formatMoney(systemCost) }}</p>
-    <p>Nilai Produksi/tahun: {{ formatMoney(yearlyValueCreation) }}</p>
-    <p>Balik Modal pada tahun ke {{ isNaN(systemPaybackPeriod) ? 0 : systemPaybackPeriod.toString().slice(0,3) }}</p>
+        <option disabled value="">Publik (Subsidi):</option>
+        <option v-bind:value="0.685">P-1/450 VA</option>
+        <option v-bind:value="0.76">P-1/900 VA</option>
+        <option v-bind:value="1.049">P-1/1300 VA</option>
+        <option v-bind:value="1.076">P-1/2200 VA</option>
+        <option v-bind:value="1.076">P-1/3500 VA</option>
+        <option v-bind:value="1.076">P-1/4400 VA</option>
+        <option v-bind:value="1.076">P-1/5500 VA</option>
+      </select>
+      <br />
+      <p>Total Biaya Sistem: <strong>{{ formatMoney(systemCost) }}</strong> (cost)</p>
+      <p>Nilai Produksi Daya/tahun: <strong>{{ formatMoney(yearlyValueCreation) }}</strong> (income)</p>
+      <p>Balik Modal pada tahun ke <strong>{{ isNaN(systemPaybackPeriod) ? 0 : systemPaybackPeriod.toString().slice(0,3) }}</strong></p>
+    </div>
   </div>
 </template>
 
@@ -135,8 +137,5 @@ export default {
 }
 </script>
 
-<style scoped>
-.center {
-  text-align: center;
-}
+<style>
 </style>
